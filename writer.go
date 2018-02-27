1c2ae8a012c1e54691c1c286c9d1910dce6aac4e
karupanerura <karupa@cpan.org>
2018-02-27 11:59:55 +0900

    fix

--- writer.go
+++ writer.go
@@ -12,7 +12,7 @@ type Writer struct {
 }
 
 func NewWriter(ctx context.Context, logger func(ctx context.Context, format string, args ...interface{})) *Writer {
-	return &Writer{ctx: ctx, l: logger}
+	return &Writer{ctx: ctx, l: Logger(logger)}
 }
 
 func (w *Writer) Write(p []byte) (n int, err error) {
